22033a287aa607c14dcba0bc36525e6b3570a4da
Syune Sargsyan <syusargsyan17@gmail.com>
2026-03-25 21:24:06 +0400

    Update MyStack.cs

--- MyStackProj/MyStack.cs
+++ MyStackProj/MyStack.cs
@@ -11,11 +11,6 @@ public class MyStack<T> : IEnumerable<T>
 
     public bool IsEmpty => Count == 0;
 
-
-    public MyStack()
-    {
-    }
-
     public MyStack(ICollection<T> collection)
     {
         foreach (var item in collection)
