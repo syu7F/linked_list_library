36105374fa64f10e4a2f50bfcc0df2c0d93934fe
css <fgfdhdfzhd>
2026-05-20 23:27:20 +0400

    added

--- MyChessProj/Program.cs
+++ MyChessProj/Program.cs
@@ -1,103 +1,138 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class Program
+namespace MyChessProject;
+
+class MyChess
 {
     static void Main()
     {
-        PrintMatrixDiaganal();
-        Console.WriteLine();
-        PrintMatrixAuxiliary();
-        Console.WriteLine();
-        Console.WriteLine(PrintRook(2, 4, 3, 7));
-        Console.WriteLine();
-        Console.WriteLine(PrintKnight(1, 2, 5, 8));
+        int n = 8;
+
+        Console.WriteLine("Գլխավոր անկյունագիծ:");
+        PrintMainDiagonal(n);
+ 
+
+        Console.WriteLine("Երկրորդական անկյունագիծ:");
+        PrintSecondDiagonal(n);
+
+
+        // Նավակի քայլի ստուգում
+        Console.WriteLine(PrintCanRookMove(2, 3, 5, 3));
+
+
+        // Ձիու մեկ քայլի ստուգում
+        Console.WriteLine(PrintCanKnightMove(2, 3, 4, 4));
+
+
+        // Ձիու նվազագույն քայլերը
+        int steps = GetKnightMinSteps(1, 1, 8, 8);
+        Console.WriteLine(steps);
+
+        // Փղի քայլի ստուգում
+        Console.WriteLine(DiagonalBishop(0, 7, 4, 3, 2, 5));
+
+        Console.ReadKey();
     }
 
-    #region Diaganal
-    static void PrintMatrixDiaganal()
+    static void PrintMainDiagonal(int matrixSize)
     {
-        int n = 5;
-        char[,] matrix = new char[n, n];
-
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < matrixSize; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < matrixSize; j++)
             {
-                if (i == j)
-                    matrix[i, j] = '#';
-                else
-                    matrix[i, j] = '*';
+                Console.Write(i == j ? "# " : "* ");
             }
-        }
-
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix[i, j] + " ");
-            }
-
             Console.WriteLine();
         }
     }
-    #endregion Diaganal
 
-    #region Auxiliary
-
-
-    static void PrintMatrixAuxiliary()
+    static void PrintSecondDiagonal(int matrixSize)
     {
-        int n = 4;
-        char[,] matrix = new char[n, n];
-
-
-        for (int i = 0; i < n; i++)
+        for (int x0 = 0; x0 < matrixSize; x0++)
         {
-            for (int j = 0; j < n; j++)
+            for (int y0 = 0; y0 < matrixSize; y0++)
             {
-                if (i + j == n - 1)
-                    matrix[i, j] = '#';
-                else
-                    matrix[i, j] = '*';
+                Console.Write(x0 + y0 == matrixSize - 1 ? "# " : "* ");
             }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix[i, j] + " ");
-            }
-
             Console.WriteLine();
         }
     }
-    #endregion Auxiliary
 
-    #region Rook
-    static bool PrintRook(int x0, int y0, int x1, int y1)
+    static bool PrintCanRookMove(int x0, int y0, int x1, int y1)
     {
-        if ((x0 == x1) || (y0 == y1))
-            return true;
-
-        else
+        if (x0 == x1 && y0 == y1)
             return false;
 
+        return x0 == x1 || y0 == y1;
     }
 
-    #endregion Rook
-
-    #region Knight
-    static bool PrintKnight(int x0, int x1, int y0, int y1)
+    static bool PrintCanKnightMove(int x0, int y0, int x1, int y1)
     {
-        if (Math.Abs(x0 - x1) * Math.Abs(y0 - y1) == 2)
-            return true;
-        else
-            return false;
+        int dx = Math.Abs(x0 - x1);
+        int dy = Math.Abs(y0 - y1);
 
-
+        return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
     }
-    #endregion Knight
 
+    static int GetKnightMinSteps(int x0, int y0, int x1, int y1)
+    {
+        int[] dx = { 2, 2, -2, -2, 1, 1, -1, -1 };
+        int[] dy = { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+        Queue<(int x, int y, int dist)> queue = new Queue<(int, int, int)>();
+        queue.Enqueue((x0, y0, 0));
+
+        bool[,] visited = new bool[9, 9];
+        visited[x0, y0] = true;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (current.x == x1 && current.y == y1)
+                return current.dist;
+
+            for (int i = 0; i < 8; i++)
+            {
+                int nextX = current.x + dx[i];
+                int nextY = current.y + dy[i];
+
+                if (nextX >= 1 && nextX <= 8 &&
+                    nextY >= 1 && nextY <= 8 &&
+                    !visited[nextX, nextY])
+                {
+                    visited[nextX, nextY] = true;
+                    queue.Enqueue((nextX, nextY, current.dist + 1));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    static bool DiagonalBishop(int x0, int y0, int x1, int y1, int obstacleX, int obstacleY)
+    {
+        if (Math.Abs(x0 - x1) == Math.Abs(y0 - y1))
+        {
+            if (Math.Abs(obstacleX - x1) == Math.Abs(obstacleY - y1))
+            {
+                if (obstacleX > Math.Min(x1, x0) &&
+                    obstacleX < Math.Max(x1, x0))
+                {
+                    if (obstacleY > Math.Min(y1, y0) &&
+                        obstacleY < Math.Max(y1, y0))
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                    return true;
+            }
+            else
+                return true;
+        }
+
+        return false;
+    }
 }
